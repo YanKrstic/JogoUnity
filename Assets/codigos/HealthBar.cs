ff99b579c0c53bd1f8bc5cbf49f3a62bc87401df
Yan Braz Krstic <131937783+YanKrstic@users.noreply.github.com>
2025-12-02 09:30:23 -0300

    Barra de vida funcionando

--- Assets/codigos/HealthBar.cs
+++ Assets/codigos/HealthBar.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     private Image healthBarFill;
+
     void Start()
     {
         healthBarFill = this.GetComponent<Image>();
     }
+
     public void UpdateHealthBar(float currentHealth, float maxHealth)
     {
         healthBarFill.fillAmount = currentHealth / maxHealth;
     }
-
 }
